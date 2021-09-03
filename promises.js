223b231f5a9a3e06fbfbede9e770bf6ccf908247
Troy Dorman <troydorman@gmail.com>
2021-09-02 19:31:58 -0600

    combined async/await with fetch

--- promises.js
+++ promises.js
@@ -53,3 +53,13 @@ async function init() {
 }
 
 init();
+
+// async / await with Fetch
+async function fetchUsers() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const data = await response.json();
+  console.log(data);
+}
+
+fetchUsers();

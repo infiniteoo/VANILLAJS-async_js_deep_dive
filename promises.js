8596ae009a9dd037a482684baa7b065e368d6399
Troy Dorman <troydorman@gmail.com>
2021-09-02 19:28:11 -0600

    covered the use of Promise.all to resolve multiple promises at once

--- promises.js
+++ promises.js
@@ -17,7 +17,7 @@ function createPost(post) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       posts.push(post);
-      const error = true;
+      const error = false;
       if (!error) {
         resolve();
       } else {
@@ -27,6 +27,20 @@ function createPost(post) {
   });
 }
 
-createPost({ title: "Post Three", body: "This is post three" })
+/* createPost({ title: "Post Three", body: "This is post three" })
   .then(getPosts)
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err)); */
+
+// Promise.all
+const promise1 = Promise.resolve("Hello World");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 2000, "Goodbye");
+});
+const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (res) => res.json()
+);
+
+Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
+  console.log(values)
+);
